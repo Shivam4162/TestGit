7573d872c0316ee48ca83c269859486bd8ad6204
Shivam <shivamprasad1867@gmail.com>
2021-01-11 19:31:37 +0530

    save changes

--- GitTesting/src/HelloWorld.java
+++ GitTesting/src/HelloWorld.java
@@ -3,10 +3,7 @@ public class HelloWorld {
 	//this is the first line
 	//this is the second line.
 	//this is third line.
-	//this is fourth line.
-<<<<<<< HEAD
-=======
+	//this is fourth line
 	//this is sexth line.
 	//this is 7th line.
->>>>>>> FirstBranch
 }
